cc10c43eb361a801497c458bb9f65a2d1d49a0b6
Hafez Md. Asir Abrar Khan <navidabid92@gmail.com>
2026-07-27 11:59:42 +0600

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 // PostgreSQL Connection Setup
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
