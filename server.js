3cab5202f7c3fcec8ca522e9e8843f71a40ef691
Hafez Md. Asir Abrar Khan <navidabid92@gmail.com>
2026-07-29 20:21:23 +0600

    Update server.js

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
+app.set('trust proxy', 1);
+
 /* =========================
    DATABASE CONNECTION
 ========================= */
